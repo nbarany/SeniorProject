6d7fb61f99cdc12cc28ae34c90e7b80dea82357a
nbarany <nickbarany@gmail.com>
2015-03-14 22:20:46 -0700

    Update SeniorProjectMain.cpp

--- SeniorProjectMain.cpp
+++ SeniorProjectMain.cpp
@@ -1,10 +1,14 @@
-# SeniorProject
-
-
 #include "LCDDriver.h"
 #include "RotaryEncoder.h"
 #include <avr/io.h>
 
+//menu global variable flags
+int volume = -40;
+int is_pressed = 0;
+int is_released = 0;
+int pos = 0;
+char buf[20];
+//buf[0] = '\0';
 
 int LCDscreen_init()
 {
@@ -35,7 +39,7 @@ int lcd_write_str(char buf[]) //find length of string, send 1 char at a time for
 	len = strlen(buf);
 	for(i=0; i<len; i++)
 	{
-		LCD_send_data(buf[i]);
+           LCD_send_data(buf[i]);
 	}
 	return 0;
 }
@@ -43,6 +47,78 @@ int lcd_write_str(char buf[]) //find length of string, send 1 char at a time for
 
 RotaryEncoder encoder(A2, A3);
 volatile int count = 0;
+int menu[5] = {0,0,0,0,0};
+
+void menu_update()
+{
+  switch(menu[1])
+  {
+    case 0:
+       
+       Serial.print(volume,DEC);
+       Serial.print("\n");
+       //adjust volume
+       if(pos)   //if knob is turned right or left
+      {
+        volume += pos;
+        pos = 0; 
+        
+        Serial.print(volume,DEC);
+        Serial.print("\n");
+        //temp = 10pow(volume/10)
+        //safeWriteReg((pow(10.0,(double(volume)/10.0))),I2C_ADDR_ADAU1702w,SAFELOAD_DATA1, SAFELOAD_ADDR1, ****addressofVolume****);lcd
+        //LCD_cmd_clr();
+        //delay(100);
+        sprintf(buf, "Volume: %d dB", volume);
+        lcd_write_str(buf);
+      }  
+      else if(is_pressed == 1)
+      {
+         //one menu level deeper
+         menu[0]++;   
+         menu[1]++;
+         is_pressed = 0;   
+      }
+    break;
+    case 1:
+       //Speaker Setup Branch
+       if(is_released == 1)
+       {
+         //LCD_cmd_clr(); 
+         //delay(100);
+         sprintf(buf, "Speaker Setup");
+          lcd_write_str(buf);
+          is_released = 0;     
+       }
+       //Scrolling Criteria  
+       if(pos > 0)  
+       {
+          menu[1] += pos;
+          pos = 0;
+          //LCD_cmd_clr();
+          //delay(100);
+          sprintf(buf, "Crossover");
+          lcd_write_str(buf);
+       }
+       else if(pos < 0)
+       {
+          menu[1] = 4;
+          pos = 0;
+          //LCD_cmd_clr();
+          //delay(100);
+          sprintf(buf, "Speaker Prot");
+          lcd_write_str(buf);
+       }
+       else if(is_pressed == 1)
+       {
+         menu[0]++;
+         menu[2]++;
+         is_pressed = 0;
+       }      
+    break;   
+  }    
+       
+}
 
 void setup() {
   // put your setup code here, to run once:
@@ -62,29 +138,29 @@ void setup() {
 ISR(PCINT1_vect) {
   noInterrupts();
   encoder.tick(); // just call tick() to check the state.
+  menu_update();
   interrupts();
 }
 
 //Debouncing issue, ISR keeps getting called on logic change
 ISR(PCINT2_vect) {               
    noInterrupts();
-   if(digitalRead(2) == 0)   //active low button
+   if(digitalRead(2) == 0)   //active low button aka press down
    {
-     count++;
+     is_pressed = 1;
+     menu_update();
    }
-   else
+   else                      // button release 
    {
-     count--;
+     is_released = 1;
+     menu_update();
    }
    interrupts();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  char buf[20];
-  buf[0] = '\0';
   
-  static int pos = 0;
 
   int newPos = encoder.getPosition();
   if (newPos != pos) {
@@ -94,10 +170,4 @@ void loop() {
     
   }
   
-  sprintf(buf, "%d , %d", pos , count);
-  lcd_write_str(buf);
-  _delay_ms(100);
-  LCD_cmd_clr();
 }
-
-
